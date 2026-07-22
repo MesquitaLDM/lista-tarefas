70d062428946b2558b74182c4cd7bef5d4f90cf3
MesquitaLDM <marcosmesquita@lojadomecanico.com.br>
2026-07-22 16:16:20 -0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -96,7 +96,8 @@ async function initDb() {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
